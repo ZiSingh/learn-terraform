ac0e408e0ef4f430d8d2ff0dad93c623e8fac831
pzite <zi@gmail.com>
2023-06-07 22:14:10 +0530

    output

--- 02-output/output.tf
+++ 02-output/output.tf
@@ -1,12 +1,20 @@
+#output "sample" {
+#  value = "Hello World"
+#}
+#
+## Any String in terraform need to be quoted with Double quotes only.
+## single quotes are not supported
+#
+#output "sample1" {
+#  value = 1000
+#}
+#
+## Numbers and booleans (true, false)
 output "sample" {
-  value = "Hello World"
+  value = "Hellow World"
 }
-
-# Any String in terraform need to be quoted with Double quotes only.
-# single quotes are not supported
 
 output "sample1" {
   value = 1000
-}
 
-# Numbers and booleans (true, false)
+}
