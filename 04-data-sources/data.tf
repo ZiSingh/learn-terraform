85c2ad6d8cf3d7a3ffea53d08d3430e0af670a8f
pzite <zi@gmail.com>
2023-06-08 17:29:42 +0530

    datasource

--- 04-data-sources/data.tf
+++ 04-data-sources/data.tf
@@ -13,25 +13,25 @@ data "aws_ec2_spot_price" "example" {
 ## now i need to print the output spot price
 
 output "price" {
-  value = data.aws_ec2_spot_price.example.spot_price.
+  value = data.aws_ec2_spot_price.example.spot_price
 }
 
 ## now i need to print the id of the AWS Region
 
 output "AWS Region" {
-  value = data.aws_ec2_spot_price.example.availability_zone.
+  value = data.aws_ec2_spot_price.example.availability_zone
 }
 
 #spot_price_timestamp - The timestamp at which the Spot Price value was published.
 
 output "spot_price_timestamp"{
-  value = data.aws_ec2_spot_price.example.spot_price_timestamp.
+  value = data.aws_ec2_spot_price.example.spot_price_timestamp
 }
 
 #info about the provider the Spot Price value was published.
 
 output "provider's details" {
-  value = data.aws_ec2_spot_price.example.provider.
+  value = data.aws_ec2_spot_price.example.provider
 
 }
 
