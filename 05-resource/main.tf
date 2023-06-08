70b2251504804e477f06094a50dfb33f68b7a92d
pzite <zi@gmail.com>
2023-06-08 22:39:13 +0530

    resource-launch an instance

--- 05-resource/main.tf
+++ 05-resource/main.tf
@@ -1,15 +1,22 @@
 #how do i launch an AWS EC2 instance using ami id
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.example.id
-  instance_type = "t3.micro"
+#resource "aws_instance" "web" {
+#  ami           = data.aws_ami.example.id
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "HelloWorld"
+#  }
+#}
+#
+#data "aws_ami" "example" {
+#  owners      = ["973714476881"]
+#  most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+#}
 
-  tags = {
-    Name = "HelloWorld"
-  }
-}
+resource "github_repository" "Zsingh-repo-1" {
+  name        = "Zsingh-repo-1"
+  description = "Zi Singh awesome codebase"
 
-data "aws_ami" "example" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
+  visibility = "public"
 }
--- 05-resource/providers.tf
+++ 05-resource/providers.tf
@@ -11,9 +11,3 @@ provider "github" {
   token = "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"
   owner = "zsingh"
 }
-resource "github_repository" "Zsingh-repo-1" {
-  name        = "Zsingh terraform-demo-1"
-  description = "Zi Singh awesome codebase"
-
-  visibility = "public"
-}
