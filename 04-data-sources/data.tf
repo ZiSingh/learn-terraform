a597446bfc6870f8c85481c6d203257d44a9db04
pzite <zi@gmail.com>
2023-06-08 17:40:47 +0530

    datasource

--- 04-data-sources/data.tf
+++ 04-data-sources/data.tf
@@ -24,7 +24,7 @@ output "AWS_Region" {
 
 #spot_price_timestamp - The timestamp at which the Spot Price value was published.
 
-output "spot_price_timestamp"{
+output "spot_price_timestamp" {
   value = data.aws_ec2_spot_price.example.spot_price_timestamp
 }
 
