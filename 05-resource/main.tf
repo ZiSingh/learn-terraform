b7309fa11cf9a89c001557d311f83e3cf74e0f3d
pzite <zi@gmail.com>
2023-06-08 22:45:45 +0530

    github repository

--- 05-resource/main.tf
+++ 05-resource/main.tf
@@ -30,6 +30,6 @@ resource "github_repository" "example" {
   template {
     owner                = "github"
     repository           = "terraform-template-module"
-    include_all_branches = true
+
   }
 }
