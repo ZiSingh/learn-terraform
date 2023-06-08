b1f509ed5246d55e6a9a37929463ac15a1731c78
pzite <zi@gmail.com>
2023-06-08 22:12:15 +0530

    resource-launch an instance

---
+++ 05-resource/main.tf
@@ -0,0 +1,15 @@
+#how do i launch an AWS EC2 instance using ami id
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
