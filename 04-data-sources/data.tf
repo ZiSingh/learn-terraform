505fa6e936c8dcc38ab7b422c70579bd20cffb56
pzite <zi@gmail.com>
2023-06-08 21:59:38 +0530

    datasource-ami id

--- 04-data-sources/data.tf
+++ 04-data-sources/data.tf
@@ -68,12 +68,3 @@ output "ami" {
 
 
 
-data "aws_ami" "example" {
-    owners      = ["973714476881"]
-    most_recent = true
-    name_regex  = "Centos-8-DevOps-Practice"
-  }
-
-output "ami" {
-  value = data.aws_ami.example.id
-}
