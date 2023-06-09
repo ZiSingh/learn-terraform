c14cf3c7c2b4ab5a6e219155180e08677df2e8d4
pzite <zi@gmail.com>
2023-06-09 13:54:39 +0530

    aws bucket

--- 09-Remote-state/main.tf
+++ 09-Remote-state/main.tf
@@ -1,20 +1,20 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "terraform-b73"
-#    key    = "sample/terraform.tfstate"
-#    region = "us-east-1"
-#  }
-#}
-#
-#
-#output "demo" {
-#  value = "Hello World"
-#}
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+terraform {
+  backend "s3" {
+    bucket = "terraform-b73"
+    key    = "sample/terraform.tfstate"
+    region = "us-east-1"
   }
 }
+
+
+output "demo" {
+  value = "Hello World"
+}
+#resource "aws_s3_bucket" "example" {
+#  bucket = "my-tf-test-bucket"
+#
+#  tags = {
+#    Name        = "My bucket"
+#    Environment = "Dev"
+#  }
+#}
