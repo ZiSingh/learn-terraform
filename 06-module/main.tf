4229ce769727cbebefb1c2e1d013a6e01e8c6d83
pzite <zi@gmail.com>
2023-06-09 07:28:16 +0530

    module create 2 ec2

--- 06-module/main.tf
+++ 06-module/main.tf
@@ -1,10 +1,4 @@
-#how do i create a 2 EC2 instance on AWS as Sample1 & Sample2
-module "Sample1" {
+#how do i create a single EC2 instance on AWS
+module "ec2" {
   source = "./ec2"
-  name = "sample1"
-}
-
-module "Sample2" {
-  source = "./ec2"
-  name = "sample2"
 }
