77b3577d87bee79a0f579a1889f53c980d9c8b55
pzite <zi@gmail.com>
2023-06-09 07:12:26 +0530

    module

--- 05-resource/main.tf
+++ 05-resource/main.tf
@@ -1,18 +1,18 @@
 #how do i launch an AWS EC2 instance using ami id
 #resource "aws_instance" "web" {
-#  ami           = data.aws_ami.example.id
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "HelloWorld"
-#  }
-#}
-#
-#data "aws_ami" "example" {
-#  owners      = ["973714476881"]
-#  most_recent = true
-#  name_regex  = "Centos-8-DevOps-Practice"
-#}
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
 
 #resource "github_repository" "Zsingh-repo-1" {
 #  name        = "Zsingh-repo-1"
@@ -21,15 +21,15 @@
 #  visibility = "public"
 #}
 
-resource "github_repository" "ZiSingh" {
-  name        = "ZiSingh"
-  description = "My awesome codebase"
-
-  visibility = "public"
-
-  template {
-    owner                = "github"
-    repository           = "terraform-template-module"
-
-  }
-}
+#resource "github_repository" "ZiSingh" {
+#  name        = "ZiSingh"
+#  description = "My awesome codebase"
+#
+#  visibility = "public"
+#
+#  template {
+#    owner                = "github"
+#    repository           = "terraform-template-module"
+#
+#  }
+#}
