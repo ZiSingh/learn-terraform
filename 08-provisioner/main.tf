152fb24a556b605e7166e766da0a74af412dbb61
pzite <zi@gmail.com>
2023-06-09 13:03:48 +0530

    provisioner

---
+++ 08-provisioner/main.tf
@@ -0,0 +1,32 @@
+
+#here we shall launch an instance and run the roboshop anible main.yml
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+provisioner "remote-exec" {
+
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  inline = [
+    "sudo labauto ansible",
+    "ansible-pull -i localhost, -U https://github.com/zsingh/roboshop-ansible main.yml -e env=dev -e role_name=frontend"
+  ]
+}
+
+}
+
+data "aws_ami" "example" {
+owners      = ["973714476881"]
+most_recent = true
+name_regex  = "Centos-8-DevOps-Practice"
+}
