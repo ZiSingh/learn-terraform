4cfce499fac8e236447790327b110fbbcb5bfe0b
pzite <zi@gmail.com>
2023-06-09 12:48:40 +0530

    module create 2 ec2

--- 07-module-1/main.tf
+++ 07-module-1/main.tf
@@ -8,3 +8,11 @@ module "Sample2" {
   source = "./ec2"
   name = "sample2"
 }
+
+output "public_ip_sample1" {
+  value = module.sample1.public_ip
+}
+
+output "public_ip_sample2" {
+  value = module.sample2.public_ip
+}
