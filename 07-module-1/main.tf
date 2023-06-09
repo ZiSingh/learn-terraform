68e499fbee61a278c7875f5700b8cc8fa656efe6
pzite <zi@gmail.com>
2023-06-09 12:50:06 +0530

    module create 2 ec2

--- 07-module-1/main.tf
+++ 07-module-1/main.tf
@@ -1,13 +1,14 @@
 #how do i create a 2 EC2 instance on AWS as Sample1 & Sample2
-module "Sample1" {
+module "sample1" {
   source = "./ec2"
-  name = "sample1"
+  name   = "sample1"
 }
 
-module "Sample2" {
+module "sample2" {
   source = "./ec2"
-  name = "sample2"
+  name   = "sample2"
 }
+
 
 output "public_ip_sample1" {
   value = module.sample1.public_ip
