e3bc0bad9d79556cea5c2a435683161aa1370a90
pzite <zi@gmail.com>
2023-06-21 18:28:20 +0530

    create aws instance

--- 01-demo/sample.tf
+++ 01-demo/sample.tf
@@ -1,7 +1,7 @@
 //How do i create a AWS instance
 //i need to have an ami id
 resource "aws_instance" "web" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = "ami-0920f062733cc174a"
   instance_type = "t3.micro"
 
   tags = {
