358e4034cf1022b968897ffee7db957d13f8e6ce
pzite <zi@gmail.com>
2023-06-21 18:25:09 +0530

    terraform variables

--- 01-demo/sample.tf
+++ 01-demo/sample.tf
@@ -1,5 +1,7 @@
+//How do i create a AWS instance
+//i need to have an ami id
 resource "aws_instance" "web" {
-  ami           = "ami-03265a0778a880afb"
+  ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
   tags = {
