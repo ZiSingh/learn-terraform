4d248c4a704987659cc077a990a4f42df9fd4cc0
pzite <zi@gmail.com>
2023-06-23 18:14:02 +0530

    learn terraform

--- 04-data-sources/data.tf
+++ 04-data-sources/data.tf
@@ -10,23 +10,7 @@ data "aws_ec2_spot_price" "example" {
   }
 }
 
-## now i need to print the output spot price
 
-output "price" {
-  value = data.aws_ec2_spot_price.example.spot_price
-}
-
-## now i need to print the id of the AWS Region
-
-output "AWS_Region" {
-  value = data.aws_ec2_spot_price.example.availability_zone
-}
-
-#spot_price_timestamp - The timestamp at which the Spot Price value was published.
-
-output "spot_price_timestamp" {
-  value = data.aws_ec2_spot_price.example.spot_price_timestamp
-}
 
 #info about the provider the Spot Price value was published.
 #
@@ -38,33 +22,33 @@ output "spot_price_timestamp" {
 #  value = data.aws_ec2_spot_price.example.provider
 #}
 
-output "Instance_Type" {
-  value = data.aws_ec2_spot_price.example.instance_type
-}
-
-# to display the security group id
-
-data "aws_security_group" "selected" {
-  name = "allow-all"
-}
-
-output "sgid" {
-  value = data.aws_security_group.selected.id
-}
-
-#to display the ami id here owner=account number of aws
-
-data "aws_ami" "example" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-}
-
-output "ami" {
-  value = data.aws_ami.example.id
-}
-
-#how do i print the ami id of my when i have ami name
+#output "Instance_Type" {
+#  value = data.aws_ec2_spot_price.example.instance_type
+#}
+#
+## to display the security group id
+#
+#data "aws_security_group" "selected" {
+#  name = "allow-all"
+#}
+#
+#output "sgid" {
+#  value = data.aws_security_group.selected.id
+#}
+#
+##to display the ami id here owner=account number of aws
+#
+#data "aws_ami" "example" {
+#  owners      = ["973714476881"]
+#  most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+#}
+#
+#output "ami" {
+#  value = data.aws_ami.example.id
+#}
+#
+##how do i print the ami id of my when i have ami name
 
 
 
---
+++ 04-data-sources/output.tf
@@ -0,0 +1,17 @@
+## now i need to print the output spot price
+
+output "price" {
+  value = data.aws_ec2_spot_price.example.spot_price
+}
+
+## now i need to print the id of the AWS Region
+
+output "AWS_Region" {
+  value = data.aws_ec2_spot_price.example.availability_zone
+}
+
+#spot_price_timestamp - The timestamp at which the Spot Price value was published.
+
+output "spot_price_timestamp" {
+  value = data.aws_ec2_spot_price.example.spot_price_timestamp
+}
