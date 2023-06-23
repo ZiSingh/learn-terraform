b8cc3565ada259685d9f189427c128db1ce8fe6c
pzite <zi@gmail.com>
2023-06-23 17:44:09 +0530

    learn terraform

--- 03-vars/output.tf
+++ 03-vars/output.tf
@@ -1,59 +1,60 @@
-#//these variables will take the values from vars.tf
-#
-#output "sample" {
-#  value = var.sample
-#}
-#
-#output "sample1" {
-#  value = var.sample1
-#}
-#
-#output "Sample-Ext" {
-#  value = "The value of Sample is ${var.sample} & the value of Sample1 is ${var.sample1}"
-#}
-#
-#output "Course" {
-#  value = var.Course
-#}
-#
-#output "Courses" {
-#  value = var.Courses
-#}
-#
-#output "Course_Details" {
-#  value = var.Course_Details
-#}
-#
-##now i want to display the courses in serial wise
-#
-#output "First_Course" {
-#  value = var.Courses[0]
-#}
-#
-#output "Second_Course" {
-#  value = var.Courses[1]
-#
-#}
-#
-#output "Third_Course" {
-#  value = var.Courses[2]
-#}
+//these variables will take the values from vars.tf
+
+output "sample" {
+  value = var.sample
+}
+
+output "sample1" {
+  value = var.sample1
+}
+
+output "Sample-Ext" {
+  value = "The value of Sample is ${var.sample} & the value of Sample1 is ${var.sample1}"
+}
+
+output "Course" {
+  value = var.Course
+}
+
+output "Courses" {
+  value = var.Courses
+}
+
+output "Course_Details" {
+  value = var.Course_Details
+}
+
+#now i want to display the courses in serial wise
+
+output "First_Course" {
+  value = var.Courses[0]
+}
+
+output "Second_Course" {
+  value = var.Courses[1]
+
+}
+
+output "Third_Course" {
+  value = var.Courses[2]
+}
 
 #now i want to display the courses details of a particular course
 
 
-#output "course_details_DevOps" {
-#  value = var.Course_Details["DevOps"]
-#}
-#
-#output "course_details_AWS" {
-#  value = var.Course_Details["AWS"]
-#}
-#
-#output "course_details_Python" {
-#  value = var.Course_Details["Python"]
-#}
+output "course_details_DevOps" {
+  value = var.Course_Details["DevOps"]
+}
 
+output "course_details_AWS" {
+  value = var.Course_Details["AWS"]
+}
+
+output "course_details_Python" {
+  value = var.Course_Details["Python"]
+}
+
+#now i want to display the courses details of a particular course without naming it
 output "course_details_DevOps" {
   value = var.Course_Details[var.Courses[0]]
 }
