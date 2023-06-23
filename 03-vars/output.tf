7b362a2c75363d999bf2609c7c135dd51f83d740
pzite <zi@gmail.com>
2023-06-23 16:01:41 +0530

    learn terraform

--- 03-vars/output.tf
+++ 03-vars/output.tf
@@ -1,7 +1,13 @@
+//these variables will take the values from vars.tf
+
 output "sample" {
   value = var.sample
 }
 
 output "sample1" {
   value = var.sample1
+}
+
+output "Sample-ext" {
+  value = "Value of Sample - var.sample"
 }
